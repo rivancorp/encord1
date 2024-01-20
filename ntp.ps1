700a5499b72fcb5cb06ffc352a52eac082c464da
rivancorp <52347916+rivancorp@users.noreply.github.com>
2024-01-20 09:26:04 +0800

    Add files via upload

---
+++ ntp.ps1
@@ -0,0 +1,5 @@
+﻿Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\w32time\TimeProviders\NtpServer"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\w32time\TimeProviders\NtpServer" -Name "Enabled" -Value 1
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Config" -Name "AnnounceFlags" -Value 5
+Restart-Service w32Time
+New-NetFirewallRule `  -Name "NTP Server Port" `  -DisplayName "NTP Server Port" `  -Description 'Allow NTP Server Port' `  -Profile Any `  -Direction Inbound `  -Action Allow `  -Protocol UDP `  -Program Any `  -LocalAddress Any `  -LocalPort 123
